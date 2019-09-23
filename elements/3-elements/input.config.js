d6acc344067bbc38b9f0a6d8aebbff375d74d075
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-23 14:49:01 -0700

    create input element

---
+++ elements/3-elements/input.config.js
@@ -0,0 +1,22 @@
+// ***** Input Element ***** //
+
+module.exports = {
+  label: 'Input',
+  context: {
+    input: {
+      id: 'test-id',
+      name: 'test-name',
+      type: 'text',
+      value: 'test-value',
+      placeholder: '',
+      list: '',
+      pattern: '',
+      min: '',
+      max: '',
+      checked: false,
+      multiple: false,
+      disabled: false,
+      required: false
+    }
+  }
+}
