26d33247ac7cca2c46497450207ac8292f3f0d34
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-24 16:23:16 -0700

    create grant id component

---
+++ elements/2-components/grantid.config.js
@@ -0,0 +1,15 @@
+// ***** Grant ID Component ***** //
+
+module.exports = {
+  label: 'Grant ID',
+  context: {
+    label: {
+      for: 'c-grantid',
+      text: 'Grant ID'
+    },
+    input: {
+      id: 'c-grantid',
+      type: 'number'
+    }
+  }
+}
