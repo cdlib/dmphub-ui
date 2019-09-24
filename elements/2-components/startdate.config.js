f9a2e029f3e5d046e1e4dd87fb892533c003a03a
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-24 14:19:07 -0700

    create start date component

---
+++ elements/2-components/startdate.config.js
@@ -0,0 +1,17 @@
+// ***** Start Date Component ***** //
+
+module.exports = {
+  label: 'Start Date',
+  context: {
+    label: {
+      for: 'c-startdate',
+      text: 'Start Date',
+      required: true
+    },
+    input: {
+      id: 'c-startdate',
+      type: 'date',
+      required: true
+    }
+  }
+}
