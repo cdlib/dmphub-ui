92da1bf3a5d64cb5b7a5585a3e8fd4ae976ee9c8
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-24 14:24:20 -0700

    create end date component

---
+++ elements/2-components/enddate.config.js
@@ -0,0 +1,17 @@
+// ***** End Date Component ***** //
+
+module.exports = {
+  label: 'End Date',
+  context: {
+    label: {
+      for: 'c-enddate',
+      text: 'End Date',
+      required: true
+    },
+    input: {
+      id: 'c-enddate',
+      type: 'date',
+      required: true
+    }
+  }
+}
