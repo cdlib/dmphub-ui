cfeef1c2d27da3b2cd0370521a15b2ff319a9e2e
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-24 14:14:35 -0700

    create abstract component

---
+++ elements/2-components/abstract.config.js
@@ -0,0 +1,14 @@
+// ***** Abstract Component ***** //
+
+module.exports = {
+  label: 'Abstract',
+  context: {
+    label: {
+      for: 'c-abstract',
+      text: 'Abstract',
+    },
+    textarea: {
+      id: 'c-abstract'
+    }
+  }
+}
