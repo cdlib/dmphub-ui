b28affd6457281d102afa74e1cd7b9430cc73f06
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-26 11:44:09 -0700

    create report component

---
+++ elements/2-components/report.config.js
@@ -0,0 +1,15 @@
+// ***** Report Component ***** //
+
+module.exports = {
+  label: 'Report',
+  context: {
+    label: {
+      for: 'c-report',
+      text: 'Report'
+    },
+    input: {
+      id: 'c-report',
+      type: 'url'
+    }
+  }
+}
