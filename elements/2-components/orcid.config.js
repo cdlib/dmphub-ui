2c75a32165b8e7f55be66cb338358bb79cca016f
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-26 14:08:16 -0700

    create orcid component

---
+++ elements/2-components/orcid.config.js
@@ -0,0 +1,13 @@
+// ***** Orcid Component ***** //
+
+module.exports = {
+  label: 'Orcid',
+  context: {
+    orcid: {
+      id: 'c-orcid',
+      href: 'https://orcid.org/0000-0001-5727-2427',
+      text: 'https://orcid.org/0000-0001-5727-2427',
+      required: true
+    }
+  }
+}
