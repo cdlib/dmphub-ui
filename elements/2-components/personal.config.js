900ce447416c2b9931d516b8887ee51d09a60e96
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-26 14:50:33 -0700

    create personal data component

---
+++ elements/2-components/personal.config.js
@@ -0,0 +1,15 @@
+// ***** Personal Data Component ***** //
+
+module.exports = {
+  label: 'Personal',
+  context: {
+    label: {
+      for: 'c-personal',
+      text: 'Contains Personal data?'
+    },
+    input: {
+      id: 'c-personal',
+      type: 'checkbox'
+    }
+  }
+}
