97f80787272a278c04d701d702525b733283ac91
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-26 14:50:07 -0700

    create description 2 component

---
+++ elements/2-components/description2.config.js
@@ -0,0 +1,14 @@
+// ***** Description 2 Component ***** //
+
+module.exports = {
+  label: 'Description 2',
+  context: {
+    label: {
+      for: 'c-description2',
+      text: 'Description',
+    },
+    textarea: {
+      id: 'c-description2'
+    }
+  }
+}
