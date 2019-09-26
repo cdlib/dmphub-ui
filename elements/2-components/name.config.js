87e576151ef56f468bd5f744b2cc0645962b7a0b
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-26 11:59:26 -0700

    create name component

---
+++ elements/2-components/name.config.js
@@ -0,0 +1,17 @@
+// ***** Name Component ***** //
+
+module.exports = {
+  label: 'Name',
+  context: {
+    label: {
+      for: 'c-name1',
+      text: 'Name',
+      required: true
+    },
+    input: {
+      id: 'c-name1',
+      type: 'text',
+      required: true
+    }
+  }
+}
