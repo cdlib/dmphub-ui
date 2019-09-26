bd94637e07d5abc4ae2aacf381e32c9abdadd01e
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-26 11:16:23 -0700

    create ethical component

---
+++ elements/2-components/ethical.config.js
@@ -0,0 +1,15 @@
+// ***** Ethical Component ***** //
+
+module.exports = {
+  label: 'Ethical',
+  context: {
+    label: {
+      for: 'c-ethical',
+      text: 'Any ethical concerns?'
+    },
+    input: {
+      id: 'c-ethical',
+      type: 'checkbox'
+    }
+  }
+}
