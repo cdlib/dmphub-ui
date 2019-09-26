b7346a5dc272e6a531a445907f708e421bf03e8a
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-26 14:42:21 -0700

    create title 2 component

---
+++ elements/2-components/title2.config.js
@@ -0,0 +1,17 @@
+// ***** Title 2 Component ***** //
+
+module.exports = {
+  label: 'Title 2',
+  context: {
+    label: {
+      for: 'c-title2',
+      text: 'Title',
+      required: true
+    },
+    input: {
+      id: 'c-title2',
+      type: 'text',
+      required: true
+    }
+  }
+}
