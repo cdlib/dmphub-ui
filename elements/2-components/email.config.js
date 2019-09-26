4a69b064637529400ee56a90822922ec40b8e1e3
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-26 12:02:13 -0700

    create email component

---
+++ elements/2-components/email.config.js
@@ -0,0 +1,17 @@
+// ***** Email Component ***** //
+
+module.exports = {
+  label: 'Email',
+  context: {
+    label: {
+      for: 'c-email',
+      text: 'Email',
+      required: true
+    },
+    input: {
+      id: 'c-email',
+      type: 'email',
+      required: true
+    }
+  }
+}
