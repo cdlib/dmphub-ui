bf5a5413833e6f8647f4a91c2126a9d71da7ad9c
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-26 16:27:46 -0700

    create button elements

---
+++ elements/3-elements/button.config.js
@@ -0,0 +1,38 @@
+// ***** Button Elements ***** //
+
+module.exports = {
+  collated: true,
+  default: '1',
+  label: 'Buttons',
+  context: {
+    button: {
+      id: 'button-id1',
+      text: 'Generic',
+      arialabel: '',
+      disabled: false,
+      hidden: false
+    }
+  },
+  variants: [
+    {
+      name: '2', // button--2.hbs
+      context: {
+        button: {
+          id: 'button-id2',
+          text: 'Add',
+          arialabel: 'Add'
+        }
+      }
+    },
+    {
+      name: '3', // button--3.hbs
+      context: {
+        button: {
+          id: 'button-id3',
+          text: 'Remove',
+          arialabel: 'Remove'
+        }
+      }
+    }
+  ]
+}
