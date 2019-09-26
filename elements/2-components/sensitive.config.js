4115bc49dc9c239d7829b7e135aede2d1dcc5495
JoelUCOP <joel.hagedorn@ucop.edu>
2019-09-26 14:53:03 -0700

    create sensitive data component

---
+++ elements/2-components/sensitive.config.js
@@ -0,0 +1,15 @@
+// ***** Sensitive Data Component ***** //
+
+module.exports = {
+  label: 'Sensitive',
+  context: {
+    label: {
+      for: 'c-sensitive',
+      text: 'Contains sensitive data?'
+    },
+    input: {
+      id: 'c-sensitive',
+      type: 'checkbox'
+    }
+  }
+}
