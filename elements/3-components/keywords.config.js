a554b4ceb48d2cc90595dfb63ecc51b7dea826b6
JoelUCOP <joel.hagedorn@ucop.edu>
2019-10-04 14:09:53 -0700

    create keywords component

---
+++ elements/3-components/keywords.config.js
@@ -0,0 +1,15 @@
+// ***** Keywords Component ***** //
+
+module.exports = {
+  label: 'Keywords',
+  context: {
+    label: {
+      for: 'c-keywords',
+      text: 'Keywords',
+    },
+    input: {
+      id: 'c-keywords',
+      type: 'text',
+    }
+  }
+}
