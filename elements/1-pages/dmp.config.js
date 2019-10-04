b16425ac5ee8b19b8653f40b1ba959bb54bdb943
JoelUCOP <joel.hagedorn@ucop.edu>
2019-10-04 13:53:06 -0700

    create dmp page

---
+++ elements/1-pages/dmp.config.js
@@ -0,0 +1,14 @@
+// ***** DMP Page ***** //
+
+module.exports = {
+  label: 'DMP',
+  context: {
+    heading: {
+      level: '2',
+      text: 'Data Management Plan'
+    },
+    intro: {
+      text: 'Tell us about your plan for managing your research outputs:'
+    }
+  }
+}
