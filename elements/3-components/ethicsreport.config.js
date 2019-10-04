4ada0cec110fcdf11fe313df2a8a9822b6ba74f6
JoelUCOP <joel.hagedorn@ucop.edu>
2019-10-04 13:40:04 -0700

    create ethics report component

---
+++ elements/3-components/ethicsreport.config.js
@@ -0,0 +1,11 @@
+// ***** Ethics Report Component ***** //
+
+module.exports = {
+  label: 'Ethics Report',
+  context: {
+    ethicsreport: {
+      id: 'component-id',
+      arialabelledby: 'checkbox-id'
+    }
+  }
+}
