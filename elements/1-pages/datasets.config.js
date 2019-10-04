be502d8e17519b295466bdf84116ca595f410c2c
JoelUCOP <joel.hagedorn@ucop.edu>
2019-10-04 14:28:11 -0700

    create dataset page

---
+++ elements/1-pages/datasets.config.js
@@ -0,0 +1,14 @@
+// ***** Datasets Page ***** //
+
+module.exports = {
+  label: 'Datasets',
+  context: {
+    heading: {
+      level: '2',
+      text: 'Datasets'
+    },
+    button: {
+      text: 'Add a dataset'
+    }
+  }
+}
