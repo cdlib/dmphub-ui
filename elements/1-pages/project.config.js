d5a7087ca3f8d0b132d95ed2a0767878da17dc4c
JoelUCOP <joel.hagedorn@ucop.edu>
2019-10-04 11:41:06 -0700

    create project page

---
+++ elements/1-pages/project.config.js
@@ -0,0 +1,14 @@
+// ***** Project Page ***** //
+
+module.exports = {
+  label: 'Project',
+  context: {
+    heading: {
+      level: '2',
+      text: 'Project'
+    },
+    intro: {
+      text: 'Tell us about your research project:'
+    }
+  }
+}
