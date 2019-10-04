c366c6bff9236370964994379cce9c013cd9896a
JoelUCOP <joel.hagedorn@ucop.edu>
2019-10-04 12:25:44 -0700

    create primary contact component

---
+++ elements/3-components/primary.config.js
@@ -0,0 +1,15 @@
+// ***** Primary Contact Component ***** //
+
+module.exports = {
+  label: 'Primary Contact',
+  context: {
+    primary: {
+      id: 'primary-id',
+      arialabel: 'primary-fieldgroup-label',
+      label: {
+        id: 'primary-fieldgroup-label',
+        text: 'Primary Contact'
+      }
+    }
+  }
+}
