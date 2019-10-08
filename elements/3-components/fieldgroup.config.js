5f980e2384ea98d12e831c9951e8999de86daf7a
JoelUCOP <joel.hagedorn@ucop.edu>
2019-10-08 11:44:51 -0700

    create field group component

---
+++ elements/3-components/fieldgroup.config.js
@@ -0,0 +1,16 @@
+// ***** Field Group Component ***** //
+
+module.exports = {
+  label: 'Field Group',
+  context: {
+    fieldgroup: {
+      id: 'fieldgroup-id',
+      arialabel: 'the-label',
+      sample: '(form elements go here)',
+      label: {
+        id: 'the-label',
+        text: 'Field Group Label'
+      }
+    }
+  }
+}
