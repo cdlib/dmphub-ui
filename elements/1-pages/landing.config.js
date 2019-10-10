37889b73493576a7b85d5c47d1c5f2b2bfbfbff2
JoelUCOP <joel.hagedorn@ucop.edu>
2019-10-10 15:12:49 -0700

    add accordions and metadata to landing page

--- elements/1-pages/landing.config.js
+++ elements/1-pages/landing.config.js
@@ -16,7 +16,12 @@ module.exports = {
       abstract: 'Mosquitoes are the deadliest animals on the planet, due to the many diseases they transmit. In the US, the most common mosquito-borne disease is West Nile virus (WNV), transmitted byCulex mosquitoes. In urban environments, improperly draining storm-water infrastructure, such as catch basins and culverts, which contain polluted stagnant water, provide ideal habitat for these mosquitoes, creating localized foci of WNV transmission risk. Our project focuses on surveying and identifying mosquito habitats in the City of Spokane and the City of Cheney, and understanding the relationship between environmental conditions, mosquito productivity and WNV transmission risk. Faculty from the Department of Biology at Eastern Washington University (Krisztian Magori, Camille McNeely and Jenifer Walke) and from Gonzaga University(Christy Andrade) will collaborate with the Spokane Regional Health District (SRDH), the Washington Department of Health (WADOH) Zoonotic Disease program, and other stakeholders in this project. During the summer, a graduate student from EWU will lead undergraduate students to check catch basins and culverts for standing water, take measurements and water samples, and monitor for mosquito larvae. If mosquito breeding is detected, they will set traps and collect mosquitoes, and send them to WADOH for WNV testing, communicating the risk to SRDH. Water samples and mosquito larvae will be tested for both nutrient and microbial content to identify the combination of nutrients and microbes that support mosquito productivity. The project will also identify specific locations that contribute to significant mosquito production and WNV transmission, allowing SRDH and other stakeholders to monitor and mitigate them.',
       name: 'Kristian Magori',
       organization: 'University of California, Berkeley',
-      funding: 'United States Geological Survey'
+      funding: 'United States Geological Survey',
+      type: 'Spreadsheet',
+      summary: 'An overview of the dataset',
+      title: 'DataOne copy of the Analysis of Readings',
+      format: 'Spreadsheet',
+      size: '1000'
     }
   }
 }
