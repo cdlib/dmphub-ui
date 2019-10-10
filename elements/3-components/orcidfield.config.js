c83d491c769af92c36e79eeb08f77d5d95830c36
JoelUCOP <joel.hagedorn@ucop.edu>
2019-10-10 10:47:36 -0700

    create orcid field component

---
+++ elements/3-components/orcidfield.config.js
@@ -0,0 +1,15 @@
+// ***** Orcid Field Component ***** //
+
+module.exports = {
+  label: 'Orcid Field',
+  context: {
+    label: {
+      for: 'orcid-field',
+      text: 'ORCID',
+    },
+    input: {
+      id: 'orcid-field',
+      type: 'text',
+    }
+  }
+}
