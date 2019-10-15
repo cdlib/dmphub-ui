0d9abde258fa085a8bc54c8040b3d30930897e2a
JoelUCOP <joel.hagedorn@ucop.edu>
2019-10-15 15:53:11 -0700

    add accordion js

--- js/main.js
+++ js/main.js
@@ -30,3 +30,31 @@ if (document.querySelector('#c-ethical')) {
     ethicalToggle()
   })
 }
+
+// ***** Accordion Component ***** //
+
+function accordion (accordionEls) {
+  accordionEls.forEach(function (el, index) {
+    if (index === 0) {
+      el.open = true
+    }
+
+    el.addEventListener('click', function () {
+      accordionEls.forEach(function (el) {
+        if (el !== this) {
+          el.open = false
+        }
+      }, this)
+    })
+  })
+}
+
+if (document.querySelector('.c-accordion')) {
+  var fundingAccordionEls = document.querySelectorAll('#accordion-funding details')
+  var datasetsAccordtionEls = document.querySelectorAll('#accordion-datasets details')
+  var distributionAccordionEls = document.querySelectorAll('#accordion-distribution details')
+
+  accordion(fundingAccordionEls)
+  accordion(datasetsAccordtionEls)
+  accordion(distributionAccordionEls)
+}
