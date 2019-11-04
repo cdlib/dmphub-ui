48aa61dfa3a1c92af44583ff6a9226b427e84e0d
JoelUCOP <joel.hagedorn@ucop.edu>
2019-11-04 15:18:59 -0800

    create other staff component

---
+++ elements/3-components/other.config.js
@@ -0,0 +1,18 @@
+// ***** Other Staff Component ***** //
+
+module.exports = {
+  label: 'Other Staff',
+  context: {
+    button2: {
+      button: {
+        id: 'button-add',
+        text: 'Add Another Staff'
+      }
+    },
+    button3: {
+      button: {
+        text: 'Remove This Staff'
+      }
+    }
+  }
+}
